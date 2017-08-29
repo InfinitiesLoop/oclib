d0fd38a5802193db77045d940c7f0f31de0ac35d
dareed <dareed@blizzard.com>
2017-08-28 21:27:12 -0700

    fixes init issues

--- init.lua
+++ init.lua
@@ -3,20 +3,21 @@ local init = {}
 function init.getfiles()
   print("initializing files...")
   local repo = nil
-  for line in io.lines("init.files") do
+  for line in io.lines(os.getenv("PWD") .. "/init.files") do
     if repo == nil do
       repo = line
       print("repo " .. repo)
     else 
       print("getting " .. line)
-      os.execute("wget https://raw.githubusercontent.com/" .. repo .. "/master/" .. line .. " " .. line)
+      os.execute("wget -f https://raw.githubusercontent.com/" .. repo .. "/master/" .. line .. " " .. line)
     end
   end
   print("done")
 end
 
 function init.clone(repo)
-  os.execute("wget https://raw.githubusercontent.com/" .. repo .. "/master/init.files init.files")
+  os.execute("wget -f https://raw.githubusercontent.com/" .. repo .. "/master/init.files init.files")
+  init.getfiles()
 end
 
 if arg[1] ~= nil then
