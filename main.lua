3a2547b3755acc68a18fbec16e12030b36f4ad27
dareed <dareed@blizzard.com>
2017-08-30 19:45:59 -0700

    todos

--- main.lua
+++ main.lua
@@ -3,3 +3,11 @@ local robot = require("robot")
 
 local q = quary.new({depth = 30, width = 200})
 q:start()
+
+
+-- todo
+-- make more durable tools
+-- script tool repair? not sure if possible. at least tool swapping so I can give it several.
+-- detect charge level?
+-- chunk loader?
+-- height support?
