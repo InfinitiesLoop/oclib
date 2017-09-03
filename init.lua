8c8d445cd99598caa7b497baec77946194cc490b
dareed <dareed@blizzard.com>
2017-09-02 22:13:58 -0700

    fixes state init.files problem

--- init.lua
+++ init.lua
@@ -1,12 +1,17 @@
 local init = {}
 local shell = require("shell")
 
-function init.getfiles()
+function init.getfiles(gotFilesList)
   print("initializing files...")
   local repo
   for line in io.lines(os.getenv("PWD") .. "/init.files") do
     if repo == nil then
       repo = line
+      if not gotFilesList then
+        -- restart, got file listing that may have changed
+        init.clone(repo, true)
+        return
+      end
       print("repo " .. repo)
     else
       print("getting " .. line)
@@ -17,15 +22,15 @@ function init.getfiles()
   print("done")
 end
 
-function init.clone(repo)
+function init.clone(repo, gotFilesList)
   os.execute("wget -f https://raw.githubusercontent.com/" .. repo .. "/master/init.files?"
     .. math.random() .. " init.files")
-  init.getfiles()
+  init.getfiles(gotFilesList)
 end
 
 local args = shell.parse( ... )
 if args[1] ~= nil then
-  init.clone(args[1])
+  init.clone(args[1], false)
 else
   init.getfiles()
 end
