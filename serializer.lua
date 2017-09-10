0b821de90868cd2c421fd74fe58cf2d64cf4ee49
dareed <dareed@blizzard.com>
2017-09-09 21:10:35 -0700

    fixes all kinds of problems deserializing lists, adds fromFile and clone

--- serializer.lua
+++ serializer.lua
@@ -2,45 +2,57 @@
 local function serialize(obj, indent, asArray)
   local s = ""
   indent = indent or ''
-  for k,v in pairs(obj) do
-    local t = type(v)
-    if t == "table" then
-      -- what kind of table, one like a list or a like a map?
-      if v[1] == nil then
-        -- map
-        if asArray then
+  if asArray then
+    for _,v in ipairs(obj) do
+      local t = type(v)
+      if t == "table" then
+        -- what kind of table, one like a list or a like a map?
+        if v[1] == nil then
+          -- map
           s = s .. indent .. t .. "="
+          s = s .. "{\n" .. serialize(v, indent .. '  ') .. indent .. "}\n"
         else
-          s = s .. indent .. k .. ":" .. t .. "="
-        end
-        s = s .. "{\n" .. serialize(v, indent .. '  ') .. indent .. "}\n"
-      else
-        -- list
-        if asArray then
+          -- list
           s = s .. indent .. "list="
-        else
-          s = s .. indent .. k .. ":list="
+          s = s .. "[\n" .. serialize(v, indent .. '  ', true) .. indent .. "]\n"
         end
-        s = s .. "[\n" .. serialize(v, indent .. '  ', true) .. indent .. "]\n"
-      end
-    elseif t == "boolean" then
-      if asArray then
+      elseif t == "boolean" then
         s = s .. indent .. t .. "="
+        if v then
+          s = s .. "true\n"
+        else
+          s = s .. "false\n"
+        end
+      else
+        s = s .. indent .. t .. "="
+        s = s .. v .. "\n"
+      end
+    end
+  else
+    for k,v in pairs(obj) do
+      local t = type(v)
+      if t == "table" then
+        -- what kind of table, one like a list or a like a map?
+        if v[1] == nil then
+          -- map
+          s = s .. indent .. k .. ":" .. t .. "="
+          s = s .. "{\n" .. serialize(v, indent .. '  ') .. indent .. "}\n"
+        else
+          -- list
+          s = s .. indent .. k .. ":list="
+          s = s .. "[\n" .. serialize(v, indent .. '  ', true) .. indent .. "]\n"
+        end
+      elseif t == "boolean" then
+        s = s .. indent .. k .. ":" .. t .. "="
+        if v then
+          s = s .. "true\n"
+        else
+          s = s .. "false\n"
+        end
       else
         s = s .. indent .. k .. ":" .. t .. "="
+        s = s .. v .. "\n"
       end
-      if v then
-        s = s .. "true\n"
-      else
-        s = s .. "false\n"
-      end
-    else
-      if asArray then
-        s = s .. indent .. t .. "="
-      else
-        s = s .. indent .. k .. ":" .. t .. "="
-      end
-      s = s .. v .. "\n"
     end
   end
   return s
@@ -53,41 +65,42 @@ local function deserializeFromLines(lines, asArray)
   local i = 1
   repeat
     local line = lines[i]
-    local indent, k, t, v
-    if asArray then
-      k = i
-      indent, t, v = string.match(line, '(%s*)([^=]+)=(.*)')
-    else
-      indent, k, t, v = string.match(line, '(%s*)([^:%s]+):(.+)=(.*)')
+    if string.len(line) > 0 then
+      local indent, k, t, v
+      if asArray then
+        indent, t, v = string.match(line, '(%s*)([^=]+)=(.*)')
+      else
+        indent, k, t, v = string.match(line, '(%s*)([^:%s]+):(.+)=(.*)')
+      end
+      if t == "string" then
+        v = v
+      elseif t == "number" then
+        v = tonumber(v)
+      elseif t == "boolean" then
+        v = v == "true"
+      elseif t == "table" then
+        -- find all the lines containing this nested table
+        local tableLines = {}
+        repeat
+          i = i + 1
+          line = lines[i]
+          tableLines[#tableLines+1] = line
+        until (line == (indent .. "}"))
+        table.remove(tableLines, #tableLines)
+        v = deserializeFromLines(tableLines)
+      elseif t == "list" then
+        -- find all the lines containing this nested list
+        local listLines = {}
+        repeat
+          i = i + 1
+          line = lines[i]
+          listLines[#listLines+1] = line
+        until (line == (indent .. "]"))
+        table.remove(listLines, #listLines)
+        v = deserializeFromLines(listLines, true)
+      end
+      if asArray then result[#result+1] = v else result[k] = v end
     end
-    if t == "string" then
-      result[k] = v
-    elseif t == "number" then
-      result[k] = tonumber(v)
-    elseif t == "boolean" then
-      result[k] = v == "true"
-    elseif t == "table" then
-      -- find all the lines containing this nested table
-      local tableLines = {}
-      repeat
-        i = i + 1
-        line = lines[i]
-        tableLines[#tableLines+1] = line
-      until (line == (indent .. "}"))
-      table.remove(tableLines, #tableLines)
-      result[k] = deserializeFromLines(tableLines)
-    elseif t == "list" then
-      -- find all the lines containing this nested list
-      local listLines = {}
-      repeat
-        i = i + 1
-        line = lines[i]
-        listLines[#listLines+1] = line
-      until (line == (indent .. "]"))
-      table.remove(listLines, #listLines)
-      result[k] = deserializeFromLines(listLines, true)
-    end
-
     i = i + 1
   until i > #lines
 
@@ -103,8 +116,28 @@ local function deserialize(str)
   return deserializeFromLines(lines)
 end
 
+local function clone(o)
+  return deserialize(serialize(o))
+end
+
+local function deserializeFile(path)
+  local l = io.lines(path)
+  if not l then
+    return nil, "could not open file"
+  end
+
+  local lines = {}
+  for line in l do
+    lines[#lines+1] = line
+  end
+
+  return deserializeFromLines(lines)
+end
+
 return {
   serialize = serialize,
   deserialize = deserialize,
   deserializeLines = deserializeFromLines,
+  deserializeFile = deserializeFile,
+  clone = clone
 }
