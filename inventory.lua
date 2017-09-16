24ccb6688bea4a59fc9d8376cb3730bfd4a0332e
dareed <dareed@blizzard.com>
2017-09-15 21:05:00 -0700

    lazy loading

--- inventory.lua
+++ inventory.lua
@@ -26,22 +26,22 @@ function inventory.dropAll(side, fromSlotNumber, exceptFor)
   local couldDropAll = true
   exceptFor = exceptFor or {}
   fromSlotNumber = fromSlotNumber or 1
-  for i=fromSlotNumber,robot.inventorySize() do
-    local c = robot.count(i)
+  for i=fromSlotNumber,robot().inventorySize() do
+    local c = robot().count(i)
     if c > 0 then
-      local stack = ic.getStackInInternalSlot(i)
+      local stack = ic().getStackInInternalSlot(i)
       if not inventory.isOneOf(stack, exceptFor) then
 
-        robot.select(i)
+        robot().select(i)
         if side == nil or side == sides.front then
-          robot.drop()
+          robot().drop()
         elseif side == sides.bottom then
-          robot.dropDown()
+          robot().dropDown()
         elseif side == sides.top then
-          robot.dropUp()
+          robot().dropUp()
         end
         -- see if all the items were successfully dropped
-        c = robot.count(i)
+        c = robot().count(i)
         if c > 0 then
           -- at least one item couldn't be dropped.
           -- but we keep trying to drop all so we still drop as much as we can.
@@ -70,10 +70,10 @@ function inventory.isIdealTorchSpot(x, z)
 end
 
 function inventory.selectItem(pattern)
-  for i=1,robot.inventorySize() do
-    local stack = ic.getStackInInternalSlot(i)
+  for i=1,robot().inventorySize() do
+    local stack = ic().getStackInInternalSlot(i)
     if stack ~= nil and string.find(stack.name, pattern) ~= nil then
-      robot.select(i)
+      robot().select(i)
       return true
     end
   end
@@ -84,9 +84,9 @@ function inventory.placeTorch(sideOfRobot, sideOfBlock)
   if inventory.selectItem("torch$") then
     local success
     if sideOfRobot == nil or sideOfRobot == sides.down then
-      success = robot.placeDown(sideOfBlock or sides.bottom)
+      success = robot().placeDown(sideOfBlock or sides.bottom)
     elseif sideOfRobot == sides.front then
-      success = robot.place(sideOfBlock or sides.bottom)
+      success = robot().place(sideOfBlock or sides.bottom)
     end
     if success then
       return true
@@ -97,8 +97,8 @@ end
 
 function inventory.isLocalFull()
   -- backwards cuz the later slots fill up last
-  for i=robot.inventorySize(),1,-1 do
-    local stack = ic.getStackInInternalSlot(i)
+  for i=robot().inventorySize(),1,-1 do
+    local stack = ic().getStackInInternalSlot(i)
     if stack == nil then
       return false
     end
@@ -107,7 +107,7 @@ function inventory.isLocalFull()
 end
 
 function inventory.toolIsBroken()
-  local d = robot.durability()
+  local d = robot().durability()
   d = util.trunc(d or 0, 2)
   return d <= 0
 end
@@ -118,19 +118,19 @@ end
 
 function inventory.pickUpFreshTools(sideOfRobot, toolName)
   sideOfRobot = sideOfRobot or sides.bottom
-  local size = ic.getInventorySize(sideOfRobot)
+  local size = ic().getInventorySize(sideOfRobot)
   if size == nil then
     return false
   end
 
   local count = 0
   for i=1,size do
-    local stack = ic.getStackInSlot(sideOfRobot, i)
+    local stack = ic().getStackInSlot(sideOfRobot, i)
     -- is this the tool we want and fully repaired?
     if inventory.stackIsItem(stack, toolName) and stack.damage == 0 then
       -- found one, get it!
-      robot.select(1) -- select 1 cuz it will fill into an empty slot at or after that
-      if not ic.suckFromSlot(sideOfRobot, i) then
+      robot().select(1) -- select 1 cuz it will fill into an empty slot at or after that
+      if not ic().suckFromSlot(sideOfRobot, i) then
         return false
       end
       count = count + 1
@@ -142,8 +142,8 @@ end
 function inventory.dropBrokenTools(sideOfRobot, toolName)
   sideOfRobot = sideOfRobot or sides.bottom
   local brokenToolsCount = 0
-  for i=1,robot.inventorySize() do
-    local stack = ic.getStackInInternalSlot(i)
+  for i=1,robot().inventorySize() do
+    local stack = ic().getStackInInternalSlot(i)
 
     if inventory.stackIsItem(stack, toolName) then
       -- is this a broken tool?
@@ -151,9 +151,9 @@ function inventory.dropBrokenTools(sideOfRobot, toolName)
       if isBroken then
         brokenToolsCount = brokenToolsCount + 1
         -- drop it
-        robot.select(i)
-        local result = (sideOfRobot == sides.bottom and robot.dropDown(1)) or
-          (sideOfRobot == sides.front and robot.drop(1))
+        robot().select(i)
+        local result = (sideOfRobot == sides.bottom and robot().dropDown(1)) or
+          (sideOfRobot == sides.front and robot().drop(1))
         if not result then
           return false, brokenToolsCount
         end
@@ -161,21 +161,21 @@ function inventory.dropBrokenTools(sideOfRobot, toolName)
     end
   end
   -- finally we need to see if the tool we are holding is broken
-  robot.select(1)
-  ic.equip()
-  local stack = ic.getStackInInternalSlot(1)
+  robot().select(1)
+  ic().equip()
+  local stack = ic().getStackInInternalSlot(1)
   if inventory.stackIsItem(stack, toolName) then
     -- is this a broken tool?
     local isBroken = util.trunc((stack.maxDamage-stack.damage) / stack.maxDamage, 2) <= 0
     if isBroken then
       brokenToolsCount = brokenToolsCount + 1
-      if not robot.dropDown(1) then
-        ic.equip()
+      if not robot().dropDown(1) then
+        ic().equip()
         return false, brokenToolsCount
       end
     end
   end
-  ic.equip()
+  ic().equip()
   return true, brokenToolsCount
 end
 
@@ -184,11 +184,11 @@ function inventory.equipFreshTool(itemName)
     -- use the currently selected tool as the pattern.
     -- first we must see what tool it is we currently have
     -- swap it with slot 1
-    robot.select(1)
-    ic.equip()
-    local currentTool = ic.getStackInInternalSlot()
+    robot().select(1)
+    ic().equip()
+    local currentTool = ic().getStackInInternalSlot()
     -- equip it back since whatever was in slot 1 might be important
-    ic.equip()
+    ic().equip()
 
     if currentTool == nil then
       -- no current tool, sorry
@@ -198,25 +198,25 @@ function inventory.equipFreshTool(itemName)
     itemName = currentTool.name
   end
 
-  for i=1,robot.inventorySize() do
-    local stack = ic.getStackInInternalSlot(i)
+  for i=1,robot().inventorySize() do
+    local stack = ic().getStackInInternalSlot(i)
 
     if itemName == "!empty" then
       if stack == nil or (stack.maxDamage == nil or stack.maxDamage == 0) then
         -- found an empty slot or at least something that doesn't use durability
-        robot.select(i)
-        ic.equip()
+        robot().select(i)
+        ic().equip()
         return true
       end
     elseif stack ~= nil and (stack.name == itemName or string.match(stack.name, itemName)) then
-      robot.select(i)
-      ic.equip()
+      robot().select(i)
+      ic().equip()
       -- found one but we need to check if it's got durability
       if not inventory.toolIsBroken() then
         return true
       end
       -- not durable enough, so put it back and keep looking
-      ic.equip()
+      ic().equip()
     end
   end
 
